e32ae44046afc90616925184bef379373a3609f7
Porkchop <peter@peterOzolins.com>
2012-10-25 13:25:04 -0400

    this adds most structure to wedding page

--- wedding.php
+++ wedding.php
@@ -31,107 +31,67 @@
                         <!-- ### -->
                         <div id="contentContainer">
                             <div id="contentContainerInner">
-                                <div class="clearfix">
+                                <div class="columnLeft">
+                                    <div class="clearfix">
+                                    </div>
+                                    <h2>Your Perfcet Day</h2>
+                                    <h4>Your ceremony, your reception, your music.</h4>
+                                    <hr />
+                                    <div class="weddingImageBig"></div>
+                                    <h3>Professional DJ's & MC's</h3>
+                                    <p>Our experienced staff have backgrounds in television, radio, improv and stage with your team, you get 2 performers who specialize in getting your crowd excited from the moment you walk in!</p>
+
+                                    <div class="columnLeftRow">
+                                        <h3>Plan It All With Us</h3>
+                                        <div class="rowImage"></div>
+                                        <p>
+                                            Your MC focuses on your special moments. The Grand entrance, First Dance, Toasts, Cake cutting; just to start... Your DG specializes in playing the music you select, while mixing in an amazing blend of what keep your guests on the dance floor all night long.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <h3>Event Scheduling</h3>
+                                        <div class="rowImage"></div>
+                                        <p>
+                                            We do it all!!!  We work with you closely to create the perfect timeline.  Your schedule will keep include all of your formalities & tie together lots of dancing and fun so your party is moving every second. We even coordinate all of the scheduling with your other vendors!
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <h3>Have It Your Way</h3>
+                                        <div class="rowImage"></div>
+                                        <p>
+                                            Every event we do is different and unique.  Our staff is the same.  With many performers to choose from, you will find the perfect team of MC & DJ to run your event with smooth precision.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <h3>The Add-Ons</h3>
+                                        <div class="rowImage"></div>
+                                        <p>
+                                            Aside from music we also provide some incredible ADD-ONs for your special day...Photobooths, Uplighting, or a personalized mongram on the dance floor...It’s only the beginning of what we can provide.
+                                        </p>
+                                    </div>
                                 </div>
-                                <div class="contentImageRight">
+                                <div class="columnRight">
+                                    <div class="sideBox">
+                                        <h3>Event Services</h3>
+                                        <hr>
+                                        <p>Bar & Batmitzvahs</p>
+                                        <p>Weddings</p>
+                                        <p>Corporate</p>
+                                        <p>Birthdays</p>
+                                        <p>Schools</p>
+                                    </div>
+                                    <div class="sideBox requestQuote">
+                                        <h3>Request a Quote</h3>
+                                    </div>
+
                                 </div>
-                                <h2>
-                                    The Vibe
-                                    <span class="dark">
-                                        What We Do
-                                    </span>
-                                </h2>
-                                <hr />
-                                <p>
-                                    You get one chance to celebrate your Bar/Bat-Mitzvah, Wedding, Corporate
-                                    Event, or special occasion.
-                                </p>
-                                <p>
-                                    <strong>
-                                        You want it done perfectly.
-                                    </strong>
-                                    With our focus on cutting edge entertainment, we always create an event
-                                    that's unique, different and customozed to your special requests.
-                                </p>
-                                <p>
-                                    With a staff of more than 20 performers, our
-                                    <strong>
-                                        Party Hosts, Pro-DGs, and Ineractive Dancers
-                                    </strong>
-                                    love coming up with ground-breaking and exiting ways to keep your party
-                                    going all night long!
-                                </p>
-                                <p>
-                                    Our persoal service is second to none. Our
-                                    <strong>
-                                        office staff, performers and event specialists
-                                    </strong>
-                                    will give you the most creative ideas, flexible pricing, and help along
-                                    the way to bring
-                                    <strong>
-                                        Real Genius
-                                    </strong>
-                                    to your nex special event.
-                                </p>
-                                <h3>
-                                    Plan It All With Us
-                                </h3>
-                                <hr />
-                                <div class="clearfix">
-                                </div>
-                                <div class="contentBottomBoxes">
-                                    <ul>
-                                        <li>
-                                            <div>
-                                            </div>
-                                            <h2 class="light">
-                                                Mitzvahs
-                                            </h2>
-                                            <p>
-                                                Wisi vestibulum. Dolor sed, sem integer vitae, ipsum quam non cras, feugiat
-                                                faucibus pede. Viverra sed sem aliquet sem.
-                                            </p>
-                                        </li>
-                                        <li>
-                                            <div>
-                                            </div>
-                                            <h2 class="light">
-                                                Weddings
-                                            </h2>
-                                            <p>
-                                                Amet purus, aliquip sit, dolor integer ut interdum praesent, mi cursus
-                                                nam tempor duis placerat volutpat. Duis non nec eu. mi cursus.
-                                            </p>
-                                        </li>
-                                        <li>
-                                            <div>
-                                            </div>
-                                            <h2 class="light">
-                                                Corporate
-                                            </h2>
-                                            <p>
-                                                Amet purus, aliquip sit, dolor integer ut interdum praesent, mi cursus
-                                                nam tempor duis placerat volutpat. Duis non nec eu dapibus. Tristique turpis.
-                                            </p>
-                                        </li>
-                                        <li>
-                                            <div>
-                                            </div>
-                                            <h2 class="light">
-                                                Special Events
-                                            </h2>
-                                            <p>
-                                                Viverra nec in hendrerit lorem vulputate, et est. Dolor nonummy. Volutpat
-                                                odio urna tellus laoreet malesuada, non
-                                            </p>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="clearfix">
-                                </div>
+                                <div class="clearfix"></div>
                             </div>
-                            <div id="contentBottomEdge">
-                            </div>
+                            <div class="clearfix"></div>
+                            <div id="contentBottomEdge"></div>
                         </div>
                     </div>
 <!-- footer -->
