bee78362e61050a1e296628fe1b6c9627cd1d749
Porkchop <peter@peterOzolins.com>
2012-10-25 16:13:31 -0400

    updates page coppied from events to make Enhancements

--- enhancements.php
+++ enhancements.php
@@ -34,59 +34,64 @@
                                 <div class="columnLeft">
                                     <div class="clearfix">
                                     </div>
-                                    <h2>Your Perfcet Day</h2>
-                                    <h4>Your ceremony, your reception, your music.</h4>
+                                    <h2>Enhancements Title</h2>
                                     <hr />
-                                    <div class="weddingImageBig"></div>
-                                    <h3>Professional DJ's & MC's</h3>
-                                    <p>Our experienced staff have backgrounds in television, radio, improv and stage with your team, you get 2 performers who specialize in getting your crowd excited from the moment you walk in!</p>
+                                    <h4>ART & AIRBRUSH - UNIQUE STYLE EVERY TIME!</h4>
 
                                     <div class="columnLeftRow">
-                                        <h3>Plan It All With Us</h3>
                                         <div class="rowImage"><img src="img/weddingDetailsRow.jpg" /></div>
                                         <p>
                                             Your MC focuses on your special moments. The Grand entrance, First Dance, Toasts, Cake cutting; just to start... Your DG specializes in playing the music you select, while mixing in an amazing blend of what keep your guests on the dance floor all night long.
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
+
                                     <div class="columnLeftRow">
-                                        <h3>Event Scheduling</h3>
                                         <div class="rowImage"><img src="/img/weddingSchedulingRow.jpg" /></div>
                                         <p>
                                             We do it all!!!  We work with you closely to create the perfect timeline.  Your schedule will keep include all of your formalities & tie together lots of dancing and fun so your party is moving every second. We even coordinate all of the scheduling with your other vendors!
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
+
+                                    <h4>GAME STATIONS - START YOUR ENGINES!</h4>
+
                                     <div class="columnLeftRow">
-                                        <h3>Have It Your Way</h3>
                                         <div class="rowImage"><img src="img/weddingDetailsRow.jpg" /></div>
                                         <p>
                                             Every event we do is different and unique.  Our staff is the same.  With many performers to choose from, you will find the perfect team of MC & DJ to run your event with smooth precision.
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
+
                                     <div class="columnLeftRow">
-                                        <h3>The Add-Ons</h3>
+                                        <div class="rowImage"><img src="/img/weddingAdOnsRow.jpg" /></div>
+                                        <p>
+                                            Aside from music we also provide some incredible ADD-ONs for your special day...Photobooths, Uplighting, or a personalized mongram on the dance floor...It’s only the beginning of what we can provide.
+                                        </p>
+                                    </div>
+                                    <div class="columnLeftRow">
                                         <div class="rowImage"><img src="/img/weddingAdOnsRow.jpg" /></div>
                                         <p>
                                             Aside from music we also provide some incredible ADD-ONs for your special day...Photobooths, Uplighting, or a personalized mongram on the dance floor...It’s only the beginning of what we can provide.
                                         </p>
                                     </div>
                                 </div>
+
+                                <!-- ##end left column / start right column -->
+
                                 <div class="columnRight">
                                     <div class="sideBox">
-                                        <h3>Event Services</h3>
+                                        <h3>ENHANCEMENTS</h3>
                                         <hr>
-                                        <p>Bar & Batmitzvahs</p>
-                                        <p>Weddings</p>
-                                        <p>Corporate</p>
-                                        <p>Birthdays</p>
-                                        <p>Schools</p>
-                                    </div>
-                                    <div class="sideBox requestQuote">
-                                        <h3>Request a Quote</h3>
-                                        <hr>
-                                        <p>make contact form</p>
+                                        <p>Airbrush & Glitter Tattoos</p>
+                                        <p class="active">Airbrush Shirts & Hats</p>
+                                        <p>Game Stations</p>
+                                        <p>GreenScreen Photos</p>
+                                        <p>Photobooths</p>
+                                        <p>Giveaways</p>
+                                        <p>Montage Production</p>
+                                        <p>LED Lighting</p>
                                     </div>
 
                                 </div>
