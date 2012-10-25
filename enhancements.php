685826569a121b5f4fd6201e10b46d82f2169445
Porkchop <peter@peterOzolins.com>
2012-10-25 15:33:57 -0400

    adds enhancements page to the site

---
+++ enhancements.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+                    <div id="bodyContainer">
+                        <?php
+                            include("includes/header_inc.php");
+                        ?>
+
+                        <!-- END HEADER -->
+                        <!-- ### -->
+                        <div id="contentContainer">
+                            <div id="contentContainerInner">
+                                <div class="columnLeft">
+                                    <div class="clearfix">
+                                    </div>
+                                    <h2>Your Perfcet Day</h2>
+                                    <h4>Your ceremony, your reception, your music.</h4>
+                                    <hr />
+                                    <div class="weddingImageBig"></div>
+                                    <h3>Professional DJ's & MC's</h3>
+                                    <p>Our experienced staff have backgrounds in television, radio, improv and stage with your team, you get 2 performers who specialize in getting your crowd excited from the moment you walk in!</p>
+
+                                    <div class="columnLeftRow">
+                                        <h3>Plan It All With Us</h3>
+                                        <div class="rowImage"><img src="img/weddingDetailsRow.jpg" /></div>
+                                        <p>
+                                            Your MC focuses on your special moments. The Grand entrance, First Dance, Toasts, Cake cutting; just to start... Your DG specializes in playing the music you select, while mixing in an amazing blend of what keep your guests on the dance floor all night long.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <h3>Event Scheduling</h3>
+                                        <div class="rowImage"><img src="/img/weddingSchedulingRow.jpg" /></div>
+                                        <p>
+                                            We do it all!!!  We work with you closely to create the perfect timeline.  Your schedule will keep include all of your formalities & tie together lots of dancing and fun so your party is moving every second. We even coordinate all of the scheduling with your other vendors!
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <h3>Have It Your Way</h3>
+                                        <div class="rowImage"><img src="img/weddingDetailsRow.jpg" /></div>
+                                        <p>
+                                            Every event we do is different and unique.  Our staff is the same.  With many performers to choose from, you will find the perfect team of MC & DJ to run your event with smooth precision.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <h3>The Add-Ons</h3>
+                                        <div class="rowImage"><img src="/img/weddingAdOnsRow.jpg" /></div>
+                                        <p>
+                                            Aside from music we also provide some incredible ADD-ONs for your special day...Photobooths, Uplighting, or a personalized mongram on the dance floor...It’s only the beginning of what we can provide.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="columnRight">
+                                    <div class="sideBox">
+                                        <h3>Event Services</h3>
+                                        <hr>
+                                        <p>Bar & Batmitzvahs</p>
+                                        <p>Weddings</p>
+                                        <p>Corporate</p>
+                                        <p>Birthdays</p>
+                                        <p>Schools</p>
+                                    </div>
+                                    <div class="sideBox requestQuote">
+                                        <h3>Request a Quote</h3>
+                                        <hr>
+                                        <p>make contact form</p>
+                                    </div>
+
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div id="contentBottomEdge"></div>
+                        </div>
+                    </div>
+<!-- footer -->
+                    <?php
+                        include("includes/footer_inc.php");
+                    ?>
+
+                    <div class="clear">
+                    </div>
+                    </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
