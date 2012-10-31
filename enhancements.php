e2669af898ac8354778c30f575adc1405c7ba6a8
Porkchop <peter@peterOzolins.com>
2012-10-31 19:42:06 -0400

    adds all enhancement rows

--- enhancements.php
+++ enhancements.php
@@ -19,45 +19,150 @@
                                     </div>
                                     <h2>Enhancements Title</h2>
                                     <hr class="enhancementsTitleLine"/>
-                                    <h4>ART & AIRBRUSH - UNIQUE STYLE EVERY TIME!</h4>
+                                    <h3>ART &amp; AIRBRUSH - UNIQUE STYLE EVERY TIME!</h3>
 
                                     <div class="columnLeftRow">
-                                        <div class="rowImage"><img src="img/weddingDetailsRow.jpg" /></div>
+                                        <div class="rowImage"><img src="img/enhancements-airbrush1.jpg" /></div>
+                                        <h4>Airbrush Tattoos</h4>
                                         <p>
-                                            Your MC focuses on your special moments. The Grand entrance, First Dance, Toasts, Cake cutting; just to start... Your DG specializes in playing the music you select, while mixing in an amazing blend of what keep your guests on the dance floor all night long.
+                                            Tons of cool themes to choose from!  Our super-talented airbrush artists spray multi-colored designs anywhere you want...Quick to dry and long-lasting, this goodie is fun for all ages.
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
 
                                     <div class="columnLeftRow">
-                                        <div class="rowImage"><img src="/img/weddingSchedulingRow.jpg" /></div>
+                                        <div class="rowImage"><img src="/img/enhancements-airbrush2.jpg" /></div>
+                                        <h4>Airbrush T-Shirts, Hats, Boxers, Tanks, etc.</h4>
                                         <p>
-                                            We do it all!!!  We work with you closely to create the perfect timeline.  Your schedule will keep include all of your formalities & tie together lots of dancing and fun so your party is moving every second. We even coordinate all of the scheduling with your other vendors!
+                                            Our awesome airbrush artists create items with flare for all of your guests to take home as a souvenir from your event.  Each item is custom designed to your specifications.
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
 
-                                    <h4>GAME STATIONS - START YOUR ENGINES!</h4>
+                                    <h3>GAME STATIONS - START YOUR ENGINES!</h3>
 
                                     <div class="columnLeftRow">
-                                        <div class="rowImage"><img src="img/weddingDetailsRow.jpg" /></div>
+                                        <div class="rowImage"><img src="img/enhancements-guitar-hero.jpg" /></div>
+                                        <h4>Guitar Hero</h4>
                                         <p>
-                                            Every event we do is different and unique.  Our staff is the same.  With many performers to choose from, you will find the perfect team of MC & DJ to run your event with smooth precision.
+                                            Kick start your party by playing your favorite rock hits in front of all of your biggest fans! Tons of songs to choose from. (2 players).
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-mario-cart.jpg" /></div>
+                                        <h4>Mario Kart</h4>
+                                        <p>
+                                            Our #1 Seller!  Kids of any age can burn up the road on 40 different courses using your favorire Mario characters  (4 players)
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
 
                                     <div class="columnLeftRow">
                                         <div class="rowImage"><img src="/img/weddingAdOnsRow.jpg" /></div>
+                                        <h4>Rockband</h4>
                                         <p>
-                                            Aside from music we also provide some incredible ADD-ONs for your special day...Photobooths, Uplighting, or a personalized mongram on the dance floor...It’s only the beginning of what we can provide.
+                                            Get ready to be famous!  Guitar, drums, & micrphone.  Up to 4, 'Rock Out' in front of a live audience!  (2-4 players)
                                         </p>
                                         <div class="clearfix"></div>
                                     </div>
                                     <div class="columnLeftRow">
-                                        <div class="rowImage"><img src="/img/weddingAdOnsRow.jpg" /></div>
+                                        <div class="rowImage"><img src="/img/enhancements-wii.jpg" /></div>
+                                        <h4>Wii Sports</h4>
                                         <p>
-                                            Aside from music we also provide some incredible ADD-ONs for your special day...Photobooths, Uplighting, or a personalized mongram on the dance floor...It’s only the beginning of what we can provide.
+                                            A multi-game with Tennis, Bowling, Golf, Boxing, and Baseball.  Completely interactive and fun for all ages!  (4 players)
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-mj.jpg" /></div>
+                                        <h4>Michael Jackson Experience</h4>
+                                        <p>
+                                            MJ's 25 greatest hits have been brought to life, as you move your whole body and dance to the beat as you follow the King of Pop!  (4 players)
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-just-dance.jpg" /></div>
+                                        <h4>Just Dance  Top POP HITS</h4>
+                                        <p>
+                                            Make this game a smash!  Dance to the beat, battle your partner, and see who has the best moves to the newest music around!  (4 players)
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-resort.jpg" /></div>
+                                        <h4>Wii Sports Resort</h4>
+                                        <p>
+                                            The sequel to Wii Sports has you on vacation playing Ping Pong, Frisbee, 3-point Shootout, and 12 other cool games!  (4 players)
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-ddr.jpg" /></div>
+                                        <h4>Dance Dance Revolution</h4>
+                                        <p>
+                                            Tap your feet to the beat against your opponent to see who's got what it takes!  The original DDR with mega-dance-pads!  (2 players)
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+
+                                    <h3>PHOTO OPPS! &mdash; WE GO BIG!!!</h3>
+
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-greenscreen.jpg" /></div>
+                                        <h4>GREENSCREEN</h4>
+                                        <p>
+                                            Unlimited 5x7 glossy photos featuring your choice of over 400 amazing backgrounds and counting!  Magazine Covers, Movie Posters, Sports, Celebrities, Exotic locations and more...Always delievered to your guests in professional looking photo jackets.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-photbooth.jpg" /></div>
+                                        <h4>MEGA BOOTH</h4>
+                                        <p>
+                                            A modern take on the old-school photobooth.  No wallet size here!  5x7 Glossys, with 4 cool pictures and a custom logo with your theme, colors, and name included.  Totally fun and great for any type of event.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-starpix.jpg" /></div>
+                                        <h4>Star Pix Photo Adventure</h4>
+                                        <p>
+                                            Choose any one of a dozen colors on a 10 ft. backdrop including hats, props, wigs, shades, bubbles, and more.  Hip photographers create unique poses for these new age Glamour Shots.  Up to 8 at a time!  Everyone takes home a cool 5X7.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+
+                                    <h3>GAME SHOW EXPERIENCE</h3>
+
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-game-show.jpg" /></div>
+                                        <h4>For KIDS & ADULTS!</h4>
+                                        <p>
+                                            Questions in every category!!!<br />Extra large HIT buttons, Neon Arrows, Bells, Buzzers, Sound Effects, 2 Microphones, and HUGE score numbers for everyone to see, make this the REAL DEAL!
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+
+                                    <h3>MISSING A TITLE!!!</h3>
+
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-projector.jpg" /></div>
+                                        <h4>Montage Projection</h4>
+                                        <p>
+                                            Using a larger than life Real Genius 10 foot stretchable projection screen our technician uses state of the art equipment (DVD player & projector), cables, and expertise to show your montage to all of your guests.
+                                        </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+
+                                    <h3>TRANSFORM YOUR ROOM!!!</h3>
+                                    <div class="columnLeftRow">
+                                        <div class="rowImage"><img src="/img/enhancements-uplighting.jpg" /></div>
+                                        <h4>Uplighting</h4>
+                                        <p>
+                                            LED uplighting is the BIGGEST partyup-trend in the world.  Our designers and technicians use over 256 cool colors to create and transform your room into something your guests will remember forever.
                                         </p>
                                     </div>
                                 </div>
