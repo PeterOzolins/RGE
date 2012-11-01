abe0ba1aca1113ce8992e2b5eb6b0898890e3b89
Porkchop <peter@peterOzolins.com>
2012-10-31 21:08:35 -0400

    adds the three header borderlines

--- includes/header_inc.php
+++ includes/header_inc.php
@@ -31,7 +31,8 @@
                             </div>
                             <div class="clearfix"></div>
                             <div id="headerImage">
-                                <img src="/img/home-top-borderline.png">
+                                <img src="/img/home-top-borderline.png" id="topHeadBorderline">
+                                <img src="/img/home-middle-borderline.png" id="midHeadBorderline">
                             </div>
                         </div>
                         <div id="headerBottom">
@@ -46,5 +47,6 @@
                                         Book Your Talent Today! 1-800-555-9999
                                     </p>
                                 </div>
+                                <img src="/img/home-bottom-borderline.png" id="bottomHeadBorderline">
                             </div>
                         </div>
