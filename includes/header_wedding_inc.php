8b1d8597fd8294dcef5ac9ea37e2d30e7e14eb6b
Porkchop <peter@peterOzolins.com>
2012-11-01 15:05:23 -0400

    added wedding-header offshoot of header.inc...added slideshow to it rough

---
+++ includes/header_wedding_inc.php
@@ -0,0 +1,108 @@
+                        <div id="headerTop">
+                            <div class="topMenuContainer">
+
+                                <ul id="topMenu" class="sf-menu">
+                                    <li>
+                                        <a href="index.php" class="pgHome">HOME</a>
+                                    </li>
+                                    <li>
+                                        <a href="wedding.php" class="pgWedding">EVENTS</a>
+                                        <ul class="subMenu">
+                                            <li><a href"#">Bar & Batmitzvahs</a></li>
+                                            <li><a href"#" class="subMenuActive">Weddings</a></li>
+                                            <li><a href"#">Corporate</a></li>
+                                            <li><a href"#">Birthdays</a></li>
+                                            <li><a href"#">Schools</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="performers.php" class="pgPerformers">PERFORMERS</a>
+                                    </li>
+                                    <li>
+                                        <a href="enhancements.php" class="pgEnhancements">ENHANCEMENTS</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="pgShoutOuts">SHOUT OUTS</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="pgContact">CONTACT</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div id="headerImage">
+                                <img src="/img/home-top-borderline.png" id="topHeadBorderline">
+
+                                <!-- ### slideshow ### -->
+                                <div id="example">
+                                    <img src="img/new-ribbon.png" width="112" height="112" alt="New Ribbon" id="ribbon">
+                                    <div id="slides">
+                                        <div class="slides_container">
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/jliba/4665625073/" title="145.365 - Happy Bokeh Thursday! | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-1.jpg" width="570" height="270" alt="Slide 1"></a>
+                                                <div class="caption" style="bottom:0">
+                                                    <p>Happy Bokeh Thursday!</p>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/stephangeyer/3020487807/" title="Taxi | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-2.jpg" width="570" height="270" alt="Slide 2"></a>
+                                                <div class="caption">
+                                                    <p>Taxi</p>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/childofwar/2984345060/" title="Happy Bokeh raining Day | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-3.jpg" width="570" height="270" alt="Slide 3"></a>
+                                                <div class="caption">
+                                                    <p>Happy Bokeh raining Day</p>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/b-tal/117037943/" title="We Eat Light | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-4.jpg" width="570" height="270" alt="Slide 4"></a>
+                                                <div class="caption">
+                                                    <p>We Eat Light</p>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/bu7amd/3447416780/" title="&ldquo;I must go down to the sea again, to the lonely sea and the sky; and all I ask is a tall ship and a star to steer her by.&rdquo; | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-5.jpg" width="570" height="270" alt="Slide 5"></a>
+                                                <div class="caption">
+                                                    <p>&ldquo;I must go down to the sea again, to the lonely sea and the sky...&rdquo;</p>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/streetpreacher/2078765853/" title="twelve.inch | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-6.jpg" width="570" height="270" alt="Slide 6"></a>
+                                                <div class="caption">
+                                                    <p>twelve.inch</p>
+                                                </div>
+                                            </div>
+                                            <div class="slide">
+                                                <a href="http://www.flickr.com/photos/aftab/3152515428/" title="Save my love for loneliness | Flickr - Photo Sharing!" target="_blank"><img src="img/slide-7.jpg" width="570" height="270" alt="Slide 7"></a>
+                                                <div class="caption">
+                                                    <p>Save my love for loneliness</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a href="#" class="prev"><img src="img/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a>
+                                        <a href="#" class="next"><img src="img/arrow-next.png" width="24" height="43" alt="Arrow Next"></a>
+                                    </div>
+                                    <img src="img/example-frame.png" width="739" height="341" alt="Example Frame" id="frame">
+                                </div>
+                                <!-- ### end slideshow ### -->
+
+                                <img src="/img/home-middle-borderline.png" id="midHeadBorderline">
+                            </div>
+                        </div>
+                        <div id="headerBottom">
+                            <div id="headerBottomInner">
+                                <div id="headerBottomInnerLeft">
+                                    <p>
+                                        Like Us!
+                                    </p>
+                                </div>
+                                <div id="headerBottomInnerRight">
+                                    <p>
+                                        Book Your Talent Today! 1-800-555-9999
+                                    </p>
+                                </div>
+                                <img src="/img/home-bottom-borderline.png" id="bottomHeadBorderline">
+                            </div>
+                        </div>
