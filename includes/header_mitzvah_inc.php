9eb43eba904eb4308afe3fe6cf8d9c13de3b0a1c
Porkchop <peter@peterOzolins.com>
2012-11-05 15:33:24 -0500

    renamed and added files to make mitzvah page possible

---
+++ includes/header_mitzvah_inc.php
@@ -0,0 +1,57 @@
+                        <div id="headerTop">
+                            <div class="topMenuContainer">
+
+                                <ul id="topMenu" class="sf-menu">
+                                    <li>
+                                        <a href="index.php" class="pgHome">HOME</a>
+                                    </li>
+                                    <li>
+                                        <a href="wedding.php" class="pgWedding">EVENTS</a>
+                                        <ul class="subMenu">
+                                            <li><a href"#">Bar & Batmitzvahs</a></li>
+                                            <li><a href"#" class="subMenuActive">Weddings</a></li>
+                                            <li><a href"#">Corporate</a></li>
+                                            <li><a href"#">Birthdays</a></li>
+                                            <li><a href"#">Schools</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="performers.php" class="pgPerformers">PERFORMERS</a>
+                                    </li>
+                                    <li>
+                                        <a href="enhancements.php" class="pgEnhancements">ENHANCEMENTS</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="pgShoutOuts">SHOUT OUTS</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="pgContact">CONTACT</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div id="headerImage">
+                                <img src="/img/home-top-borderline.png" id="topHeadBorderline">
+
+                                <!--@@@  iFrame for slideshow -->
+                                    <iframe src="/includes/slideshow_mitz.html" seamless="seamless" id="slideshowIframe"></iframe>
+                                <!--@@@  END iFrame for slideshow -->
+
+                                <img src="/img/home-middle-borderline.png" id="midHeadBorderline">
+                            </div>
+                        </div>
+                        <div id="headerBottom">
+                            <div id="headerBottomInner">
+                                <div id="headerBottomInnerLeft">
+                                    <p>
+                                        Like Us!
+                                    </p>
+                                </div>
+                                <div id="headerBottomInnerRight">
+                                    <p>
+                                        Book Your Talent Today! 1-800-555-9999
+                                    </p>
+                                </div>
+                                <img src="/img/home-bottom-borderline.png" id="bottomHeadBorderline">
+                            </div>
+                        </div>
