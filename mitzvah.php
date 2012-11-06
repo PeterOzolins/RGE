5dfacbbb06e67a0180387e5ea2ec3c839cb64758
Porkchop <peter@peterOzolins.com>
2012-11-06 17:44:59 -0500

    move events page side bar into include, remove from wedding & mitzvah

--- mitzvah.php
+++ mitzvah.php
@@ -50,40 +50,14 @@
                                     </div>
                                 </div>
 
-                                <div class="columnRight">
-                                    <div class="sideBox">
-                                        <h3>Event Services</h3>
-                                        <hr>
-                                        <p><a href="/mitzvah.php" class="subMenuActive">Bar & Batmitzvahs</a></p>
-                                        <p><a href="/wedding.php">Weddings</a></p>
-                                        <p><a href="#">Corporate</a></p>
-                                        <p><a href="#">Birthdays</a></p>
-                                        <p><a href="#">Schools</a></p>
-                                    </div>
-                                    <div class="sideBox requestQuote">
-                                        <h3>Request a Quote</h3>
-                                        <hr>
-                                        <form id="requestQuoteForm">
-                                            NAME*: <input type="text" class="nameBox"><input type="text" class="nameBox"><br />
-                                            PHONE*: <input type="text" class="phoneBox"><input type="text" class="phoneBox"><input type="text" class="phoneBox"><br />
-                                            EMAIL: <input type="text" class="emailBox"><br />
-                                            PARTY DATE: <input type="text" class="partyDateBox"><br />
-                                            TIME OF EVENT: <select>
-                                                    <option value="Afternoon">Afternoon</option>
-                                                    <option value="Evening">Evening</option>
-                                                </select><br />
-                                            TYPE OF EVENT: <select>
-                                                    <option value="Bar-Mitzvah">Bar-Mitzvah</option>
-                                                    <option value="Wedding">Wedding</option>
-                                                </select><br />
-                                            GUESTS OF HONOR:<br />
-                                            <input type="text" class="guestBox"><br />
-                                            <input type="submit" value="LETS DO IT!">
-                                            <input type="reset" value="LETS NOT DO IT">
-                                        </form>
-                                    </div>
 
-                                </div>
+                                <!-- @@@ INCLUDE RIGHT COLUM WITH
+                                            WITH EVENT SVCS BOX
+                                            AND REQUEST A QUOTE -->
+                                    <?php 
+                                        include 'includes/eventRightCol_inc.php';
+                                    ?>
+
                                 <div class="clearfix"></div>
                             </div>
                             <div class="clearfix"></div>
