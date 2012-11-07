8a9cf2e42a2b32b3e7b764df55f21c97f85754f2
Porkchop <peter@peterOzolins.com>
2012-11-07 13:41:15 -0500

    unsaved change on Right Column sidebar

--- includes/eventRightCol_inc.php
+++ includes/eventRightCol_inc.php
@@ -2,7 +2,7 @@
                                     <div class="sideBox ">
                                         <h3>Event Services</h3>
                                         <hr>
-                                        <p><a href="/mitzvah.php" class="subMitzvah">Bar & Batmitzvahs</a></p>
+                                        <p><a href="/mitzvah.php" class="subMitzvah">Bar &amp; Bat-Mitzvahs</a></p>
                                         <p><a href="/wedding.php" class="subWedding">Weddings</a></p>
                                         <p><a href="#">Corporate</a></p>
                                         <p><a href="#">Birthdays</a></p>
