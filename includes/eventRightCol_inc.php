227fdd6fb86ee7cb24a1b832c0854f05853feb56
Porkchop <peter@peterOzolins.com>
2012-11-07 14:47:59 -0500

    small updates to form

--- includes/eventRightCol_inc.php
+++ includes/eventRightCol_inc.php
@@ -1,34 +1,40 @@
-                                <div class="columnRight">
-                                    <div class="sideBox ">
-                                        <h3>Event Services</h3>
-                                        <hr>
-                                        <p><a href="/mitzvah.php" class="subMitzvah">Bar &amp; Bat-Mitzvahs</a></p>
-                                        <p><a href="/wedding.php" class="subWedding">Weddings</a></p>
-                                        <p><a href="#">Corporate</a></p>
-                                        <p><a href="#">Birthdays</a></p>
-                                        <p><a href="#">Schools</a></p>
-                                    </div>
-                                    <div class="sideBox requestQuote">
-                                        <h3>Request a Quote</h3>
-                                        <hr>
-                                        <form id="requestQuoteForm">
-                                            NAME*: <input type="text" class="nameBox"><input type="text" class="nameBox"><br />
-                                            PHONE*: <input type="text" class="phoneBox"><input type="text" class="phoneBox"><input type="text" class="phoneBox"><br />
-                                            EMAIL: <input type="text" class="emailBox"><br />
-                                            PARTY DATE: <input type="text" class="partyDateBox"><br />
-                                            TIME OF EVENT: <select>
-                                                    <option value="Afternoon">Afternoon</option>
-                                                    <option value="Evening">Evening</option>
-                                                </select><br />
-                                            TYPE OF EVENT: <select>
-                                                    <option value="Bar-Mitzvah">Bar-Mitzvah</option>
-                                                    <option value="Wedding">Wedding</option>
-                                                </select><br />
-                                            GUESTS OF HONOR:<br />
-                                            <input type="text" class="guestBox"><br />
-                                            <input type="submit" value="LETS DO IT!">
-                                            <input type="reset" value="LETS NOT DO IT">
-                                        </form>
-                                    </div>
+<div class="columnRight">
+    <div class="sideBox ">
+        <h3>Event Services</h3>
+        <hr>
+        <p><a href="/mitzvah.php" class="subMitzvah">Bar &amp; Bat-Mitzvahs</a></p>
+        <p><a href="/wedding.php" class="subWedding">Weddings</a></p>
+        <p><a href="#">Corporate</a></p>
+        <p><a href="#">Birthdays</a></p>
+        <p><a href="#">Schools</a></p>
+    </div>
+    <div class="sideBox requestQuote">
+        <h3>Request a Quote</h3>
+        <hr>
+        <form id="requestQuoteForm">
+            NAME*: <input type="text" class="nameBox"><input type="text" class="nameBox"><br />
+            PHONE*: <input type="text" class="phoneBox"><input type="text" class="phoneBox"><input type="text" class="phoneBox"><br />
+            EMAIL: <input type="text" class="emailBox"><br />
+            PARTY DATE: <input type="text" class="partyDateBox"><br />
+            TIME OF EVENT: <select>
+                    <option value="Afternoon">Afternoon</option>
+                    <option value="Evening">Evening</option>
+                </select><br />
+            TYPE OF EVENT: <select>
+                    <option value="Bat-Mitzvah">Bat-Mitzvah</option>
+                    <option value="Bar-Mitzvah">Bar-Mitzvah</option>
+                    <option value="Wedding">Wedding</option>
+                    <option value="Corporate">Corporate</option>
+                    <option value="Other">Other</option>
+                </select><br />
+            GUEST OF HONOR:<br />
+            <input type="text" class="guestBox"><br />
+            <div class="formBtnRow">
+                <input type="submit" value="LETS DO IT!">
+                <!--send to Randy@RealGeniusDJs.com -->
+                <input type="reset" value="LETS NOT">
+            </div>
+        </form>
+    </div>
 
-                                </div>
+</div>
