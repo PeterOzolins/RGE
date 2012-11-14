adf597ec9c078603c5e83d21d28d74067f3df9ae
Porkchop <peter@peterOzolins.com>
2012-11-13 20:38:54 -0500

    add style/content contact us page

--- contact.php
+++ contact.php
@@ -15,19 +15,62 @@
                         <div id="contentContainer">
                             <div id="contentContainerInner">
                                 <div class="clearfix"></div>
-                                <div class="columnLeft">
+                                <div class="columnLeft contact">
                                      <div class="clearfix"> 
                                     </div>
                                     <h1><span class="iCaps">C</span>ontact <span class="dark"><span class="iCaps">U</span>s...</span></h1>
                                     <hr />
-                                    <p>We are located in Southern California somewhere...</p>
-                                    <?php include('includes/requestQuote_inc.php'); ?>    
+                                    <p>It's time to put that AMAZING party together.  We have a staff of 3 in the office to take care of your needs, answer your questions, and find the perfect staff and Pre-Party activities to make the whole night PERFECT.</p>
+
+                                    <table>
+                                        <tr>
+                                            <td>
+                                                <h5>Office Hours</h5>
+                                            </td>
+                                            <td>
+                                                <p><strong>Monday &mdash; Thursday</strong> 10AM-5:30pm <br><strong>Friday</strong>  10AM-2pm</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>Call Us</h5>
+                                            </td>
+                                            <td>
+                                                <p><a href="tel:818-346-3482">(818) 346-3482</a></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>Email Us</h5>
+                                            </td>
+                                            <td>
+                                                <p><a href="mailto:randy@realGeniusDJs.com" target="_blank">Randy@RealGeniusDJs.com</a></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>Like Us</h5>
+                                            </td>
+                                            <td>
+                                                <p><a href="http://www.facebook.com/RealGeniusDJs" target="_blank">www.facebook.com/RealGeniusDJs</a></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>S'more Us</h5>
+                                            </td>
+                                            <td>
+                                                <p><a href="http://www.smore.com/wzkr-having-a-party" target="_blank">www.smore.com/wzkr-having-a-party</a></p>
+                                            </td>
+                                        </tr>
+                                    </table>
 
                                 </div>
                             <!--###########-->      
                                 <!-- ##end left column / start right column -->
 
                                 <div class="columnRight">
+                                    <?php include('includes/requestQuote_inc.php'); ?>    
                                     <!-- <div class="sideBox ">
                                         <p>Some stuff like an address or something like that you know...</p>
                                     </div> -->
